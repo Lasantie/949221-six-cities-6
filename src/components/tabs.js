25865e90cea54ba2c093213b26b343f469b1dfbd
Levan <levan@iremadze.net>
2021-02-17 19:50:05 +0300

    module2-task1

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,44 +1,33 @@
-import React, {PureComponent} from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
 import tabsFromDB from '../db/tabs.json';
 
-export default class Tabs extends PureComponent {
+const Tabs = () => {
+  const [tabs, setTabs] = React.useState(tabsFromDB.tabs);
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      tabs: tabsFromDB.tabs
-    };
-  }
-
-  onTabSelect(event, tabId) {
+  const onTabSelect = (event, tabId) => {
     event.preventDefault(); // Пока страниц нет, что бы не перекидывало на 404
-    const tabs = [...this.state.tabs];
     tabs.forEach((tab) => {
       tab.selected = tab.id === tabId;
     });
-    this.setState({tabs});
-  }
+    setTabs([...tabs]);
+  };
 
-  render() {
-    const {tabs} = this.state;
-    return (
-      <div className="tabs">
-        <section className="locations container">
-          <ul className="locations__list tabs__list">
-            {
-              tabs.map(({id, url, title, selected}) =>
-                <li key={id} className="locations__item">
-                  <Link to={url} className={`locations__item-link tabs__item ${selected && `tabs__item--active`}`} onClick={(event)=>this.onTabSelect(event, id)}>
-                    <span>{title}</span>
-                  </Link>
-                </li>
-              )
-            }
-          </ul>
-        </section>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="tabs">
+      <section className="locations container">
+        <ul className="locations__list tabs__list">
+          {tabs.map(({id, url, title, selected}) =>
+            <li key={id} className="locations__item">
+              <Link to={url} className={`locations__item-link tabs__item ${selected && `tabs__item--active`}`}
+                onClick={(event) => onTabSelect(event, id)}>
+                <span>{title}</span>
+              </Link>
+            </li>)}
+        </ul>
+      </section>
+    </div>
+  );
+};
 
+export default Tabs;
