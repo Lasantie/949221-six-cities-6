3dda7306aa10110dda70f8f5ddff38d24c1c589b
Levan <levan@iremadze.net>
2021-02-19 20:02:22 +0300

    module3-task1

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useMemo} from 'react';
 import PropTypes from 'prop-types';
 import withLayout from "../hocs/with-layout";
 import Offer from "../components/offer";
@@ -10,8 +10,12 @@ const Main = ({offers}) => {
 
   const [, handleCurrentOfferId] = useState();
   const [currentTabId, handleCurrentTabId] = useState(1);
+  const [optionsOpen, handleOptions] = useState(false);
 
-  const {title: cityTitle} = tabs.find((item) => item.id === currentTabId);
+  const cityTitle = useMemo(() => {
+    const {title} = tabs.find((item) => item.id === currentTabId);
+    return title;
+  }, tabs);
 
   return (
     <main className="page__main page__main--index">
@@ -24,17 +28,17 @@ const Main = ({offers}) => {
             <b className="places__found">{offers.length} places to stay in {cityTitle}</b>
             <form className="places__sorting" action="#" method="get">
               <span className="places__sorting-caption">Sort by</span>
-              <span className="places__sorting-type" tabIndex="0">
+              <span className="places__sorting-type" tabIndex="0" onClick={() => handleOptions(!optionsOpen)}>
                   &nbsp;Popular
                 <svg className="places__sorting-arrow" width="7" height="4">
                   <use xlinkHref={`#icon-arrow-select`}/>
                 </svg>
               </span>
-              <ul className="places__options places__options--custom places__options--opened">
-                <li key={`21`} className="places__option places__option--active" tabIndex="0">Popular</li>
-                <li key={`22`} className="places__option" tabIndex="0">Price: low to high</li>
-                <li key={`23`} className="places__option" tabIndex="0">Price: high to low</li>
-                <li key={`24`} className="places__option" tabIndex="0">Top rated first</li>
+              <ul className={`places__options places__options--custom ${optionsOpen && `places__options--opened`}`}>
+                <li key={`21`} className="places__option places__option--active" tabIndex="0" onClick={() => handleOptions(false)}>Popular</li>
+                <li key={`22`} className="places__option" tabIndex="0" onClick={() => handleOptions(false)}>Price: low to high</li>
+                <li key={`23`} className="places__option" tabIndex="0" onClick={() => handleOptions(false)}>Price: high to low</li>
+                <li key={`24`} className="places__option" tabIndex="0" onClick={() => handleOptions(false)}>Top rated first</li>
               </ul>
             </form>
             <div className="cities__places-list places__list tabs__content">
